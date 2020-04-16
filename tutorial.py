0aa3557c06eb6fca2db953d1e119af56edcf1780
sushagupta <63763046+sushagupta@users.noreply.github.com>
2020-04-16 08:00:55 +0530

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -30,7 +30,7 @@ default_args = {
     # 'trigger_rule': 'all_success'
 }
 dag = DAG(
-    'sampleTutorial',
+    'Tutorial',
     default_args=default_args,
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
